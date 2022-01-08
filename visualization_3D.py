7b132e0cda05e5bdeac81daf8610b7d391d9c21a
105071079 <37044030+Jhbai@users.noreply.github.com>
2022-01-09 02:15:26 +0800

    Create visualization_3D.py

---
+++ visualization_3D.py
@@ -0,0 +1,15 @@
+class Plot_3D:
+    def __init__(self, data):
+        self.data = data
+    def plot(self):
+        plt.figure(figsize = (10, 8))
+        ax = plt.gca(projection='3d')
+        X = self.data[self.data.columns[0]]
+        Y = self.data[self.data.columns[1]]
+        Z = self.data[self.data.columns[2]]
+        ax.scatter(X, Y, Z, c = Z, marker = 'o')
+        cols = self.data.columns
+        ax.set_xlabel(cols[0])
+        ax.set_ylabel(cols[1])
+        ax.set_zlabel(cols[2])
+        plt.show()
